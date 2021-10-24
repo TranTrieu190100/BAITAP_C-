551fc23a796884d430c82dbe9abf9ac43d9e5324
TranTrieu190100 <92834196+TranTrieu190100@users.noreply.github.com>
2021-10-24 19:46:51 +0700

    Update BAI59.cpp
    tìm xem năm con gì ?

--- BAI59.cpp
+++ BAI59.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 // BAI 59: NHAP VAO NAM DUNG LICH XUAT TEN NAM AM LICH 
 // XUAT NAM DUNG LICH KE TIEP CO CUNG TEN NAM AM LICH CHU KI CAN CHI 
-// goi y: nam co cung ten Âm lich với nam y la y+-k*60( 60 la BSCNN cua hai chu ki 10 va 12). Moc tinh Can Chi, lay nam 0 la nam Canh Than
+// goi y: nam co cung ten Âm lich với nam y la y+-k*60( 60 la BSCNN cua hai chu ki 10 va 12). 
+//Moc tinh Can Chi, lay nam 0 la nam Canh Than
 
 string can[10] = {"Canh","Tan","Nham","Quy","Giap","At","Binh","Dinh"};
 string chi[12] = { "Than", "Dau", "Tuat", "Hoi", "Ti", "Suu",
