04dfa988523fb28ec4470e91daf7b16e5e51d13b
TranTrieu190100 <92834196+TranTrieu190100@users.noreply.github.com>
2021-10-25 18:22:05 +0700

    Add files via upload

--- mang.cpp
+++ mang.cpp
@@ -4,10 +4,9 @@ using namespace std;
 // Nhap xuat mang
 int n;
 int a[100];
-void NhapMang()
+void NhapMang( int n, int a[] )
 {
-	cout << " Nhap mang so luong phan tu mang nho hon 100 : ";
-	cin >> n;
+	
 	for (int i = 0; i < n; i++)
 	{
 		cout << " Phan tu mang thu a[" << i << "] = ";
@@ -15,7 +14,7 @@ void NhapMang()
 	}
 
 }
-void XuatMang()
+void XuatMang(int n, int a[])
 {
 	cout << "\n Cac phan tu cua mang ban vua nhap la: ";
 	for (int i = 0; i < n; i++)
@@ -23,8 +22,30 @@ void XuatMang()
 		cout << a[i] <<" ";
 	}
 }
+// xap xepv
+void SapXep(int n, int a[])
+{
+	int tam;
+	for (int i = 0; i < n; i++)
+	{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (a[i] > a[j])
+				{
+					tam = a[i];
+					a[i] = a[j];
+					a[j] = tam;
+				}
+			}
+	}
+			
+}
+
 int main()
 {
-	NhapMang();
-	XuatMang();
+	cout << " Nhap mang so luong phan tu mang nho hon 100 : ";
+	cin >> n;
+	NhapMang(n,a);
+	SapXep(n,a);
+	XuatMang(n,a );
 }
